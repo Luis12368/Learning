304a9c10a414e7a9ffab5d6ad8532fd929ba58a5
Luis Gustavo Hauck <72949389+Luis12368@users.noreply.github.com>
2022-09-08 16:57:27 -0300

    Update

--- Python/PythonDSA/Cap03/Lab02/calculadora_v1.py
+++ Python/PythonDSA/Cap03/Lab02/calculadora_v1.py
@@ -5,5 +5,25 @@
 # Assista o vídeo com a execução do programa!
 
 print("\n******************* Python Calculator *******************")
+print("""Selecione a opção:
+1-Soma
+2-Subtração
+3-Multiplicação
+4-Divisão
+""")
+op = int(input('Digite a opção desejada: '))
+num1 = int(input('Insira um número: '))
+num2 = int(input('Insira mais um: '))
+
+if op == 1:
+    print(f'{num1} + {num2} =', num1+num2)
+elif op == 2:
+    print(f'{num1} - {num2} =', num1-num2)
+elif op == 3:
+    print(f'{num1} * {num2} =', num1*num2)
+elif op == 4:
+    print(f'{num1}  / {num2} =', num1/num2)
+else:
+    print('Digite um número válido')
 
 
